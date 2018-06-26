3c5765202742816806dfc45e4088d3fa56f40ddd
catavarela <cavarela@itba.edu.ar>
2018-06-25 23:34:43 -0300

    Contiene prototipos, reductores[] y dec de cadenavacia

---
+++ src/generador/generarCodigoC.h
@@ -0,0 +1,45 @@
+#ifndef _GENERAR_CODIGO_C_H
+#define _GENERAR_CODIGO_C_H
+
+#include "structures.h"
+
+char * cadenaVacia = "";
+
+typedef char * (* reductor)(Nodo *);
+
+reductor reductores[] = {
+  reducirNodoCadena,
+  reducirNodoConstante,
+  reducirNodoVariable,
+  reducirNodoOperacion,
+  reducirNodoCondicional,
+  reducirNodoBloque,
+  reducirNodoVacio,
+  reducirSi,
+  reducirMientras,
+  reducirRetornar,
+  reducirInstrucciones,
+  reducirInstruccion,
+  reducirNegacion,
+  reducirImprimir
+};
+
+char * reducirNodoCadena(Nodo * nodo);
+char * reducirNodoConstante(Nodo * nodo);
+char * reducirNodoVariable(Nodo * nodo);
+char * reducirNodoOperacion(Nodo * nodo);
+char * reducirNodoCondicional(Nodo * nodo);
+char * reducirNodoBloque(Nodo * nodo);
+char * reducirNodoVacio(Nodo * nodo);
+char * reducirSi(Nodo * nodo);
+char * reducirMientras(Nodo * nodo);
+char * reducirRetornar(Nodo * nodo);
+char * reducirInstrucciones(Nodo * nodo);
+char * reducirInstruccion(Nodo * nodo);
+char * reducirNegacion(Nodo * nodo);
+char * reducirImprimir(Nodo * nodo);
+
+static char * eval(Nodo * nodo);
+char * generarCodigoC(Nodo * nodo);
+
+#endif
